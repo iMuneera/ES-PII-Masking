901ae692ec20c338724632a6301928691316568f
Muneera <muneerajaber05@gmail.com>
2025-02-17 18:41:33 +0300

    blob

--- app.py
+++ app.py
@@ -134,4 +134,5 @@ def download_file(filename):
     return send_from_directory(CLEANED_DIR, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000)) 
+    app.run(host="0.0.0.0", port=port)
